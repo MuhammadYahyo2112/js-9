61f9031efd0fbed00f84c6b3972b3ebdd004c4b7
MuhammadYahyo2112 <yahyo0121@gmail.com>
2025-04-25 18:29:35 +0300

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,12 @@
+
+// Foydalanuvchidan ma'lumotlarni olish
+let ism = prompt("Ismingizni kiriting:");
+let familya = prompt("Familyangizni kiriting:");
+let yosh = prompt("Yoshingizni kiriting:");
+
+// Natijani alert bilan chiqarish
+alert(
+    "1. Ism: " + ism.toUpperCase() + "\n" +
+    "2. Familya: " + familya.toUpperCase() + "\n" +
+    "3. Yosh: " + yosh
+);
